020d8cbef50dc881c360d123ac6290b75b392e19
Matt Snider <matt.snider@alum.utoronto.ca>
2016-10-28 22:24:21 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+
+
+setup(
+    name='Flask-Healthcheck',
+    version='0.1.0',
+    description='Healthchecks for flask applications made easy',
+    author='Matt Snider',
+    author_email='matt.snider@alum.utoronto.ca',
+    url='https://github.com/matt-snider/flask-healthcheck',
+    py_modules=['flask_healthcheck'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
+
