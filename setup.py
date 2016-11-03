6195d184ab6e0a51fae3a3793849427d86a7aa99
Matt Snider <matt.snider@alum.utoronto.ca>
2016-11-03 16:50:25 +0100

    Bump version: 0.1.1 -> 0.1.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
     name='Flask-Healthcheck',
-    version='0.1.1',
+    version='0.1.2',
     description='Healthchecks for flask applications made easy',
     author='Matt Snider',
     author_email='matt.snider@alum.utoronto.ca',
